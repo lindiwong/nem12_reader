f24c86ed18d381c62d64438bf0b13769e0fcb27c
lin <llindiwong@gmail.com>
2024-12-20 20:26:21 +0800

    Initial commit

---
+++ src/main/java/assessment/tech/nem12_reader/Nem12ReaderApplication.java
@@ -0,0 +1,13 @@
+package assessment.tech.nem12_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nem12ReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nem12ReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/assessment/tech/nem12_reader/Nem12ReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package assessment.tech.nem12_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nem12ReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
