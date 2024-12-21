ed180df16b6d89917981e6aa5b9b52be95a3393a
lin <llindiwong@gmail.com>
2024-12-21 15:44:26 +0800

    feat: enhance CsvUtility to return List<String[]>
    doesn't make sense for a utility file to return void. it should return something meaningful.

--- src/main/java/assessment/tech/nem12_reader/utilities/CsvUtility.java
+++ src/main/java/assessment/tech/nem12_reader/utilities/CsvUtility.java
@@ -7,10 +7,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.function.Consumer;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class CsvUtility {
-    public static void processFile(MultipartFile file, Consumer<String[]> consumer) throws CsvProcessingException {
+    public static List<String[]> processFile(MultipartFile file) throws CsvProcessingException {
         if (file == null || file.isEmpty()) {
             throw new InvalidCsvFormatException("File is empty or null");
         }
@@ -20,7 +21,9 @@ public final class CsvUtility {
         }
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            reader.lines().map(line -> line.split(",")).forEach(consumer);
+            return reader.lines()
+                    .map(line -> line.split(","))
+                    .collect(Collectors.toList());
         } catch (IOException ex) {
             throw new CsvProcessingException("Failed to process csv file: %s".formatted(ex.getMessage()));
         }
