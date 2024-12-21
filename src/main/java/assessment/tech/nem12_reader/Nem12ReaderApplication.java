2cd26d4ce9b72369e004c297c2ce6e9532c20d2a
lin <llindiwong@gmail.com>
2024-12-21 17:33:47 +0800

    revert: changes on reader application

--- src/main/java/assessment/tech/nem12_reader/Nem12ReaderApplication.java
+++ src/main/java/assessment/tech/nem12_reader/Nem12ReaderApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Nem12ReaderApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(Nem12ReaderApplication.class, args);
     }
+    
 }
