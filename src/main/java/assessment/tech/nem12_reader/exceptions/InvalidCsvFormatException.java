ba4b81d75bd41907add390ce7465292a5127d597
lin <llindiwong@gmail.com>
2024-12-21 09:16:42 +0800

    feat: add custom exceptions for easier logging identification

---
+++ src/main/java/assessment/tech/nem12_reader/exceptions/InvalidCsvFormatException.java
@@ -0,0 +1,7 @@
+package assessment.tech.nem12_reader.exceptions;
+
+public class InvalidCsvFormatException extends RuntimeException {
+    public InvalidCsvFormatException(String message) {
+        super(message);
+    }
+}
